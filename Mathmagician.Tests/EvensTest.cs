dfef81d9185c5f6572e263f2d82be39fd9f72d9b
Katye Russell <Katye.Russell@gmail.com>
2016-08-08 18:20:20 -0700

    Add new class Even and accompanying tests (with Kate's help)

---
+++ Mathmagician.Tests/EvensTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class EvensTest
+    {
+        // can make instance of class
+        [TestMethod]
+        public void CanMakeInstanceOfEven()
+        {
+            Even myNewEven = new Even();
+            Assert.IsNotNull(myNewEven);
+        }
+        
+        // class returns a list even if 0 is passed in
+        [TestMethod]
+        public void ClassReturnsAList()
+        {
+            Even even = new Even();
+            var coolList = even.MakeEvenList(0);
+            Assert.IsInstanceOfType(coolList, typeof(List<int>));
+        }
+
+        // class returns the correct .Count
+        [TestMethod]
+        public void EvenReturnsTheRightNumberOfElements()
+        {
+            Even even = new Even();
+            var coolList = even.MakeEvenList(8);
+            Assert.AreEqual(8, coolList.Count);
+        }
+  
+        // class returns a list with the correct sequence
+        [TestMethod]
+        public void EvenReturnsAListWithTheCorrectSequence()
+        {
+            Even even = new Even();
+            List<int> rightList = new List<int> { 2, 4, 6, 8 };
+            var coolList = even.MakeEvenList(4);
+            CollectionAssert.AreEqual(coolList, rightList);
+        }
+
+
+
+    }
+}
---
+++ Mathmagician/Even.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Even
+    {
+        
+        public List<int> MakeEvenList(int num)
+        {
+            List<int> myEvenList = new List<int>();
+
+            for (int i = 1; myEvenList.Count != num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    myEvenList.Add(i);
+                }
+            }
+            return myEvenList;
+        }
+
+
+
+    }
+}
