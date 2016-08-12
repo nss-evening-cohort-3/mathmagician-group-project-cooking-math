896f11a8d25a6c1b9c94a7a4835a277ab35aeacf
Katye Russell <Katye.Russell@gmail.com>
2016-08-12 15:41:08 -0700

    Change intList to evensList (to avoid conflict in Program.cs)

--- Mathmagician/Even.cs
+++ Mathmagician/Even.cs
@@ -11,20 +11,23 @@ namespace Mathmagician
 
         public Even(int number) : base(number)
         {
+            this.evensList = new List<int>();
         }
 
+        public List<int> evensList { get; set; }
 
-        public override List<int> MakeListOfIntegers(int num)
+
+        public override List<int> MakeListOfIntegers(int numOfIntegersToWrite)
         {
 
-            for (int i = 1; intList.Count != num; i++)
+            for (int i = 1; evensList.Count != numOfIntegersToWrite; i++)
             {
                 if (i % 2 == 0)
                 {
-                    intList.Add(i);
+                   evensList.Add(i);
                 }
             }
-            return intList;
+            return evensList;
         }
 
 
