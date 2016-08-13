d32dd38eda11148b7e1420687a1a262bb35ce8af
Katye Russell <Katye.Russell@gmail.com>
2016-08-12 17:00:04 -0700

    Change 'intList' to 'fibList' to avoid scope conflicts in Program.cs, correct one failing test

--- Mathmagician/Fibonacci.cs
+++ Mathmagician/Fibonacci.cs
@@ -9,24 +9,26 @@ namespace Mathmagician
     public class Fibonacci : Integer
     {
         public Fibonacci(int number) : base(number)
-        { }
-
-        public override List<int> MakeListOfIntegers(int number)
         {
-            //intList.Insert()
-            int x = number - 2;
+            this.fibList = new List<int>();
+        }
+
+        public List<int> fibList { get; set; }
+
+        public override List<int> MakeListOfIntegers(int numOfIntegersToWrite)
+        {
+            fibList.Insert(0, 1);
             int a = 0;
             int b = 1;
-            for (int i = 0; i < x; i++)
+            for (int i = 0; fibList.Count != numOfIntegersToWrite; i++)
             {
                 int c = a + b;
-                intList.Add(a + b);
+                fibList.Add(a + b);
                 a = b;
                 b = c;
             }
-            intList.Insert(0, 0);
-            intList.Insert(1, 1);
-            return intList;
+
+            return fibList;
         }
     }
 }
